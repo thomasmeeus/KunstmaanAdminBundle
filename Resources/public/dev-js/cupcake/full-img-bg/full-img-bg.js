77274f6b0db1cc74d109e268c5b266cdf24cdd02
Ibe Vanmeenen <ibevanmeenen@hotmail.com>
2014-05-09 16:50:51 +0200

    dev

---
+++ Resources/public/dev-js/cupcake/full-img-bg/full-img-bg.js
@@ -0,0 +1,27 @@
+/* ==========================================================================
+   Full image background fallback with Backstretch
+   https://github.com/srobbin/jquery-backstretch
+
+   Initialize:
+   cupcake.backstretch.init();
+   ========================================================================== */
+
+var cupcake = cupcake || {};
+
+cupcake.backstretch = (function($, window, undefined) {
+
+    var init;
+
+    init = function() {
+        if($('.full-img-bg').length){
+            var imageUrl = $('.full-img-bg').data("backstretch-img");
+
+            $('.full-img-bg').backstretch(imageUrl);
+        }
+    };
+
+    return {
+        init: init
+    };
+
+}(jQuery, window));
