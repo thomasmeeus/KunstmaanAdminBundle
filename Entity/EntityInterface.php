b1f720a4a1744e73601533cfcd4bb25b18c2c533
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2014-08-08 16:54:07 +0200

    EntityInterface added

---
+++ Entity/EntityInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kunstmaan\AdminBundle\Entity;
+
+/**
+ * An interface for a generic entity
+ */
+interface EntityInterface
+{
+    /**
+     * @return int
+     */
+    public function getId();
+
+}
